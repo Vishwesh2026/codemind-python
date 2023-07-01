0c27744acf04334f81615331e90c113d2eb7a05e
Vishwesh2026 <119729995+Vishwesh2026@users.noreply.github.com>
2023-07-01 18:51:52 +0530

    def count_odd_between_even(arr):     count = 0     for i in range(1, len(arr)-1):         if arr[i-1] % 2 == 0 and arr[i+1] % 2 == 0 and arr[i] % 2 == 1:             count += 1     return count
    N = int(input())
    arr = list(map(int, input().split()))
    result = count_odd_between_even(arr)
    print(result)

---
+++ count_odd_numbers_between_even_numbers.py
@@ -0,0 +1,11 @@
+def count_odd_between_even(arr):
+    count = 0
+    for i in range(1, len(arr)-1):
+        if arr[i-1] % 2 == 0 and arr[i+1] % 2 == 0 and arr[i] % 2 == 1:
+            count += 1
+    return count
+
+N = int(input())
+arr = list(map(int, input().split()))
+result = count_odd_between_even(arr)
+print(result)
