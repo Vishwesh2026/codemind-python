708d00a39bb5730cf9df6fee7d70bf7a00230b87
Vishwesh2026 <119729995+Vishwesh2026@users.noreply.github.com>
2023-07-01 18:42:48 +0530

    def count_odd_between_odd(arr):     count = 0     for i in range(1, len(arr)-1):         if arr[i-1] % 2 == 1 and arr[i+1] % 2 == 1 and arr[i] % 2 == 1:             count += 1     return count
    N = int(input())
    arr = list(map(int, input().split()))
    result = count_odd_between_odd(arr)
    print(result)

---
+++ count_the_odd_numbers_inbetween_odd_numbers.py
@@ -0,0 +1,11 @@
+def count_odd_between_odd(arr):
+    count = 0
+    for i in range(1, len(arr)-1):
+        if arr[i-1] % 2 == 1 and arr[i+1] % 2 == 1 and arr[i] % 2 == 1:
+            count += 1
+    return count
+
+N = int(input())
+arr = list(map(int, input().split()))
+result = count_odd_between_odd(arr)
+print(result)
